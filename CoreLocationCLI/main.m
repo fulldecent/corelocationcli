5e222101c8d39b9dea1eb5c991c0b30d9f7621ec
William Entriken <github.com@phor.net>
2014-11-14 09:57:57 -0500

    Fixes #2, not sure why

--- CoreLocationCLI/main.m
+++ CoreLocationCLI/main.m
@@ -7,8 +7,6 @@
 @end
 
 @implementation Delegate;
-@synthesize locationManager = _locationManager;
-@synthesize once = _once;
 
 - (void)start:(BOOL)once
 {
@@ -16,13 +14,14 @@
     self.locationManager = [[CLLocationManager alloc] init];
     self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
     self.locationManager.delegate = self;
-    [self.locationManager startUpdatingLocation];
-    NSLog(@"Location: %@", self.locationManager.location);
+    
     NSLog(@"authorizationStatus: %d", [CLLocationManager authorizationStatus]);
     NSLog(@"locationServicesEnabled: %d", [CLLocationManager locationServicesEnabled]);
     NSLog(@"significantLocationChangeMonitoringAvailable: %d", [CLLocationManager significantLocationChangeMonitoringAvailable]);
     NSLog(@"headingAvailable: %d", [CLLocationManager headingAvailable]);
     NSLog(@"regionMonitoringAvailable: %d", [CLLocationManager regionMonitoringAvailable]);
+    
+    [self.locationManager startUpdatingLocation];
 }
 
 - (void)locationManager:(CLLocationManager *)manager
@@ -39,6 +38,7 @@
     printf ( "ERROR: %s\n", [[error localizedDescription] UTF8String]);
     exit(1);
 }
+
 @end
 
 int main(int argc, const char * argv[])
@@ -46,7 +46,7 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         BOOL once = (argc > 1 && strcmp(argv[1], "--once") == 0);
         Delegate *delegate = [[Delegate alloc] init];
-        [delegate performSelectorOnMainThread:@selector(start:) withObject:[NSNumber numberWithBool:once] waitUntilDone:NO];
+        [delegate performSelectorOnMainThread:@selector(start:) withObject:@(once) waitUntilDone:NO];
         NSRunLoop *runLoop = [NSRunLoop mainRunLoop];
         [runLoop run];
     }
